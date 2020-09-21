886f48e3e8ef7f5ecdf975ad098c7111f221ccb8
Arthur91S <arturs.stulpe@gmail.com>
2020-09-21 13:35:04 +0100

    latest

--- src/main/java/training/busboard/Main.java
+++ src/main/java/training/busboard/Main.java
@@ -4,19 +4,21 @@ import org.glassfish.jersey.jackson.JacksonFeature;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String args[]) {
-        // Your code here!
 
         String URL = "https://pokeapi.co/api/v2/pokemon/1";
 
         Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
-        String name = client.target(URL)
-                .request(MediaType.TEXT_PLAIN)
-                .get(String.class);
+        Object name = client.target(URL)
+                .request(MediaType.APPLICATION_JSON)
+                .get(Object.class);
+
+        int test = 1;
+
 
         System.out.println(name);
-
     }
 }	
